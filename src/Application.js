31dc98512ee3c00345b293b23968838b50f41d07
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-09-18 00:15:45 +0530

    image added

--- src/Application.js
+++ src/Application.js
@@ -8,10 +8,38 @@ export const FbrApplication = GObject.registerClass({
   vfunc_activate() {
     console.log("Hello! sanjai..");
     const window = new Gtk.ApplicationWindow({ application: this });
-    // const label = new Gtk.Label({ label: "Hello sanjai 🥷" });
-    // window.child = label;
 
-    const box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL });
+    const box = new Gtk.Box({
+      orientation: Gtk.Orientation.VERTICAL,
+      marginTop: 36,
+      marginBottom: 36,
+      marginStart: 36,
+      marginEnd: 36,
+      spacing: 18,
+      valign: Gtk.Align.CENTER
+    });
+
+    const image = new Gtk.Image({
+      iconName: 'system-file-manager-symbolic',
+      iconSize: Gtk.IconSize.LARGE
+    });
+
+    box.append(image);
+
+    const label = new Gtk.Label({
+      label: "Welcome  to  new file browser",
+      wrap: true
+    });
+
+    // Button
+    const button = new Gtk.Button({
+      label: "sanjai",
+      halign: Gtk.Align.CENTER
+    });
+
+    box.append(button);
+    box.append(label);
+
     window.child = box;
     window.present();
   }
